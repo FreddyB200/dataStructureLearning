bab052107507d2227d888442f7c8bb17cf2236ab
FreddyB200 <abs0rbmejohan3212@gmail.com>
2025-02-12 15:49:55 -0500

    agregando medidor de tiempo para saber en cuanto tiempo se generan los usuarios

--- src/main/java/com/datastructures/Main.java
+++ src/main/java/com/datastructures/Main.java
@@ -7,13 +7,25 @@ import com.datastructures.utils.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Agregando 20 users e imprimiendolos en pantalla: ");
-        List<User> users = DataGenerator.generateUsers(10000000);
+
+        long start = System.nanoTime();
+
+        List<User> users = DataGenerator.generateUsers(1000000);
         for (User user : users) {
             System.out.println("Id: " + user.getId());
             System.out.println("Name: " + user.getName());
             System.out.println("Age: " + user.getAge());
             System.out.println("----------------------------");
         }
+
+        long end = System.nanoTime();
+
+        long duration = end - start;
+        double milisecondsDuration = duration / 1000000;
+
+        System.out.println("Run time: " + duration + "ns");
+        System.out.println("Run time: " + milisecondsDuration + "ms");
+        System.out.println("Number of generate users: " + users.size());
 
 
     }
